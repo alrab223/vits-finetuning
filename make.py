3157f0a5bd604092dcf6f12c4ef6fd9e9316d5d0
alrab223 <allbear826@gmail.com>
2023-09-19 20:23:04 +0900

    一括変更

---
+++ make.py
@@ -0,0 +1,86 @@
+import argparse
+import glob
+import json
+import os
+import shutil
+
+import librosa
+from tqdm import tqdm
+import soundfile as sf
+
+import text
+from utils import load_filepaths_and_text
+
+
+def resample(speaker):
+   wavs = glob.glob("dataset/*.wav")
+   for wavfile in tqdm(wavs):
+      y, sr = librosa.load(wavfile, sr=22050, mono=True)
+      name = wavfile.split("\\")[1]
+      if not os.path.exists(f"wav/{speaker}"):
+         os.mkdir(f"wav/{speaker}")
+      sf.write(f"wav/{speaker}/{name}", y, sr, subtype="PCM_16")
+
+
+def make_config(speaker):
+   with open("configs/config.json", "r") as f:
+      config = json.load(f)
+   config["data"]["training_files"] = f"filelists/{speaker}/train.txt.cleaned"
+   config["data"]["validation_files"] = f"filelists/{speaker}/val.txt.cleaned"
+   config["speakers"] = [speaker]
+   with open(f"configs/{speaker}.json", "w") as f:
+      json.dump(config, f, indent=3)
+
+
+def val(speaker):
+   with open("dataset/voice_text.txt", encoding='utf-8')as f:
+      speaker_text = f.read().splitlines()
+   train = speaker_text[3::]
+   val = speaker_text[0:3]
+   with open("dataset/train.txt", "w", encoding='utf-8')as f:
+      for i in train:
+         f.write(i + "\n")
+
+   with open("dataset/val.txt", "w", encoding='utf-8')as f:
+      for i in val:
+         f.write(i + "\n")
+   if not os.path.exists(f"filelists/{speaker}"):
+      os.mkdir(f"filelists/{speaker}")
+   shutil.copy("dataset/train.txt", f"filelists/{speaker}/train.txt")
+   shutil.copy("dataset/val.txt", f"filelists/{speaker}/val.txt")
+
+
+def preprocess(speaker):
+   filelists = [f"filelists/{speaker}/train.txt", f"filelists/{speaker}/val.txt"]
+   for filelist in filelists:
+      print(filelist)
+      filepaths_and_text = load_filepaths_and_text(filelist)
+      for i in range(len(filepaths_and_text)):
+         original_text = filepaths_and_text[i][1]
+         cleaned_text = text._clean_text(original_text, ["japanese_cleaners"])
+         filepaths_and_text[i][1] = cleaned_text
+      new_filelist = filelist + "." + "cleaned"
+      with open(new_filelist, "w", encoding="utf-8") as f:
+         f.writelines(["|".join(x) + "\n" for x in filepaths_and_text])
+
+
+def make_model(speaker):
+   if os.path.exists(f"save_models/{speaker}") is False:
+      os.mkdir(f"save_models/{speaker}")
+   shutil.copy("save_models/D_0-p.pth", f"save_models/{speaker}/D_0-p.pth")
+   shutil.copy("save_models/G_0-p.pth", f"save_models/{speaker}/G_0-p.pth")
+
+
+def main(speaker):
+   resample(speaker)
+   make_config(speaker)
+   val(speaker)
+   preprocess(speaker)
+   make_model(speaker)
+
+
+if __name__ == "__main__":
+   parser = argparse.ArgumentParser()
+   parser.add_argument("--speaker_name", default="speaker")
+   args = parser.parse_args()
+   main(args.speaker_name)
